96bfeb645d219fade0a541d1dd33f95cfcbfa031
pipejfdv <juampi_03_33@hotmail.com>
2024-11-14 01:31:38 -0500

    archivo war

--- src/main/java/com/FunnyMind/SpringFunyMind/SpringFunyMindApplication.java
+++ src/main/java/com/FunnyMind/SpringFunyMind/SpringFunyMindApplication.java
@@ -2,12 +2,18 @@ package com.FunnyMind.SpringFunyMind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringFunyMindApplication {
+public class SpringFunyMindApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFunyMindApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringFunyMindApplication.class);
+	}
 }
